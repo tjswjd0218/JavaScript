54b698c48afa19646286895aa529c4e3ce88a57e
tjswjd0218 <tjswjd0218@gmail.com>
2024-12-08 17:05:37 +0900

    (241208)01.01 ~ 02.02

---
+++ JavaScript/practice.js
@@ -0,0 +1,10 @@
+// 원두의 이력서
+function solution(a, b) {
+    let company = a;
+    company = b;
+    return company;
+}
+
+
+console.log(solution("캠프코드", "코드캠프")); // "코드캠프"
+console.log(solution("코딩", "딩코")); // "딩코"
